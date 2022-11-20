2e6840e202e6b58259bc51b35a4f3c48692ffd16
Ankit Shubham <ankitshubham97@gmail.com>
2022-11-20 11:51:04 +0530

    feat: boilerplate nodejs express server

---
+++ src/app.ts
@@ -0,0 +1,53 @@
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+
+import Controller from './interfaces/controller.interface';
+import errorMiddleware from './middleware/error.middleware';
+
+class App {
+  public app: express.Application;
+
+  constructor(controllers: readonly Controller[]) {
+    this.app = express();
+
+    this.initializeStandardMiddlewares();
+    this.initializeControllers(controllers);
+  }
+
+  public listen(): void {
+    this.app.listen(process.env.PORT, () => {
+      console.log(`App listening on the port ${process.env.PORT}`);
+    });
+  }
+
+  public getServer(): express.Application {
+    return this.app;
+  }
+
+  private initializeStandardMiddlewares() {
+    this.app.set('trust proxy', true);
+    this.app.use(bodyParser.json());
+    this.app.use(cookieParser());
+    this.app.use(
+      helmet({
+        contentSecurityPolicy: false,
+      })
+    );
+    this.app.use(cors({origin: ['http://localhost:3001'], credentials: true}));
+  }
+
+  private initializeControllers(controllers: readonly Controller[]) {
+    this.app.use('/checks', (_, response) => response.send());
+    controllers.forEach((controller) => {
+      this.app.use('/', controller.router);
+    });
+
+    // Error Handling
+    this.app.use(errorMiddleware);
+  }
+}
+
+export default App;
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import './utils/loadEnv';
+
+import EnvService from './services/env';
+
+const services = [EnvService];
+for (const service of services) {
+  service.init();
+  console.log('initing service')
+}
+
+import App from './app';
+import { sequelize } from './models/sql/sequelize';
+import UserController from './controllers/user/user.controller';
+
+// Setup db connection and then start app
+sequelize.sync().then(() => {
+  const app = new App([
+    new UserController(),
+  ]);
+  app.listen()
+});
